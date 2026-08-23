d736b29ff46d650841c057d9a52453f228283609
Mario <mario@supermario.dk>
2026-08-23 12:23:52 +0200

    Added basic GUI

---
+++ src/gui/window.rs
@@ -0,0 +1,127 @@
+use eframe::egui;
+
+pub fn run() -> eframe::Result{
+
+    let height = 800.0;
+    let width = 800.0;
+    let title = "Path Finder Visualizer";
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([width, height]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        title,
+        options,
+        Box::new(|_cc| Ok(Box::new(PathFinderVisualizerApp::default()))),
+    )
+    
+}
+#[derive(Clone, Copy, PartialEq)]
+enum CellState{
+    Explored,
+    Unexplored,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum DrawingTool{
+    DrawWall,
+    EraseWall,
+}
+
+struct PathFinderVisualizerApp{
+    rows: usize,
+    columns: usize,
+    selected_rows: String,
+    selected_columns: String,
+    matrix: Vec<Vec<CellState>>,
+    _status: String,
+    Algorithm: String,
+    drawing_tool: DrawingTool,
+    is_drawing: bool,
+}
+
+impl Default for PathFinderVisualizerApp{
+    fn default() -> Self{
+        Self{
+            rows: 10,
+            columns: 10,
+            selected_rows: "10".to_owned(),
+            selected_columns: "10".to_owned(),
+            matrix: Vec::new(),
+            _status: String::from("Lets get started!"),
+            Algorithm: String::from("Select Algorithm"),
+            drawing_tool: DrawingTool::DrawWall,
+            is_drawing: false,
+        }
+    }
+}
+
+
+impl eframe::App for PathFinderVisualizerApp{
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame){
+        let min_width = 50.0;
+        let min_height = 50.0;
+        let cell_width = 30.0;
+        let cell_height = 30.0;
+        let horizontal_gap = -9.0;
+        let vertical_gap = 0.0;
+        let path_finding_algorithms = ["Dijkstra", "A*", "BFS", "DFS"];
+        egui::Grid::new("pathfinding_grid")
+        .spacing([horizontal_gap, vertical_gap])
+        .show(ui, |ui| {
+            for row in 0..self.rows {
+                for column in 0..self.columns {
+                    let cell_size = egui::vec2(cell_width, cell_height);
+
+                    let (rect, response) =
+                        ui.allocate_exact_size(cell_size, egui::Sense::hover());
+
+                    ui.painter()
+                        .rect_filled(rect, 0.0, egui::Color32::WHITE);
+
+                    ui.painter().rect_stroke(
+                        rect,
+                        0.0,
+                        egui::Stroke::new(1.0, egui::Color32::BLACK),
+                        egui::StrokeKind::Inside,
+                    );
+
+                    response.on_hover_text(format!(
+                        "Row: {}, Column: {}",
+                        row, column
+                    ));
+                }
+
+                ui.end_row();
+            }
+        });
+        ui.add(egui::TextEdit::singleline(&mut self.selected_rows).desired_width(min_width).hint_text("Rows"));
+        ui.add(egui::TextEdit::singleline(&mut self.selected_columns).desired_width(min_height).hint_text("Columns"));
+        let create_button = ui.button("Create Grid");
+        egui::ComboBox::from_label("Select Algorithm")
+            .selected_text("Select Algorithm")
+            .show_ui(ui, |ui| {
+                for algorithm in path_finding_algorithms.iter() {
+                    ui.selectable_value(
+                        &mut self.Algorithm,
+                        algorithm.to_string(),
+                        algorithm.to_string(),
+                    );
+                }
+            });
+
+        if create_button.clicked() {
+            if let (Ok(rows), Ok(columns)) = (
+                self.selected_rows.trim().parse::<usize>(),
+                self.selected_columns.trim().parse::<usize>(),
+            ) {
+                self.rows = rows;
+                self.columns = columns;
+            }
+        }
+
+          
+        }
+
+    }
