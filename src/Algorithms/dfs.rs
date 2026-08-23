82bc5558973d16a3cb66f964f1a74b0210d2f026
Mario <mario@supermario.dk>
2026-08-23 15:33:15 +0200

    Move get_neighbors function to a separate module for better organization

--- src/Algorithms/dfs.rs
+++ src/Algorithms/dfs.rs
@@ -26,29 +26,3 @@ impl PathfindingAlgorithm for DFS {
         }
     }
 }
-
-pub fn get_neighbors(position: Position, graph: &[Vec<CellState>]) -> Vec<Position> {
-    let left = (Some(position.0), position.1.checked_sub(1));
-
-    let right = (Some(position.0), position.1.checked_add(1));
-
-    let up = (position.0.checked_sub(1), Some(position.1));
-
-    let down = (position.0.checked_add(1), Some(position.1));
-
-    let possible_neighbors = vec![left, right, up, down];
-    let mut neighbors = Vec::new();
-
-    for neighbor in possible_neighbors {
-        if let (Some(row), Some(column)) = neighbor {
-            let row_is_in_bounds = row < graph.len();
-            let column_is_in_bounds = row_is_in_bounds && column < graph[row].len();
-
-            if column_is_in_bounds && graph[row][column] != CellState::Wall {
-                neighbors.push((row, column));
-            }
-        }
-    }
-
-    neighbors
-}
--- src/Algorithms/mod.rs
+++ src/Algorithms/mod.rs
@@ -1,5 +1,7 @@
 mod dfs;
+mod neighbors;
 mod path_findings_traits;
 
-pub use dfs::{DFS, get_neighbors};
+pub use dfs::DFS;
+pub use neighbors::get_neighbors;
 pub use path_findings_traits::{AlgorithmInfo, PathfindingAlgorithm, Position, SearchResult};
